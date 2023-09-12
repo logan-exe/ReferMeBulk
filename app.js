b386dc9cf50e88d697c7dd445f05d5666380e4d7
logan-exe <arun.11143@gmail.com>
2023-09-12 11:55:36 +0100

    added base url

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const User = require("./Models/userModal");
-const Contact = require("./Models/contactModal")
+const Contact = require("./Models/contactModal");
 const sgMail = require("@sendgrid/mail");
 const multer = require("multer");
-const streamifier = require("streamifier")
-const csv = require('csv-parser');
+const streamifier = require("streamifier");
+const csv = require("csv-parser");
 const validator = require("email-validator");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -15,228 +15,202 @@ app.use(express.json());
 
 sgMail.setApiKey(process.env.SG_MAIL_API_KEY);
 
-
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-
-app.post("/addBulkContacts", async (req, res) => {
- try{
-   const { userId } = req.query;
-  
-
-        const issuer = await User.find({ _id: userId });
-       
-
-        upload.single("file")(req, res, (error) => {
-          if (error) {
-            console.error("Error uploading file:", error);
-            return res.status(400).json({ message: "Error uploading file" });
-          }
-
-          if (!req.file) {
-            return res.status(400).json({ message: "No file uploaded" });
-          }
-
-          const fileBuffer = req.file.buffer;
-
-          const stream = streamifier.createReadStream(fileBuffer);
-          const incorrectEmails = [];
-          const FinalList = [];
-          const FilteredData = [];
-          const currentDate = new Date();
-          const processedEmails = new Set();
-
-          stream
-            .pipe(csv())
-            .on("headers", async (headers) => {
-            
-              const trimmedHeaders = headers.map((header) => header.trim());
-              const expectedHeaders = [
-                "firstname",
-                "lastname",
-                "email",
-                "phone",
-                "referral_amount",
-              ];
-              const missingHeaders = expectedHeaders.filter(
-                (header) => !trimmedHeaders.includes(header)
-              );
-           
-
-              if (missingHeaders.length > 0) {
-                return res.status(400).json({
-                  message:
-                    "Column names in csv must match column names in csv temaplate",
-                });
-              }
-            })
-            .on("data", async (data) => {
-         
-        
-
-              // Normalize the data object keys by trimming them
-              const normalizedData = Object.fromEntries(
-                Object.entries(data).map(([key, value]) => [key.trim(), value])
-              );
-
-              if (processedEmails.has(normalizedData.email)) {
-                incorrectEmails.push(normalizedData);
-                return; // Skip processing duplicate email
-              }
-              processedEmails.add(normalizedData.email);
-              const isValidEmail = validator.validate(
-                normalizedData.email
-              );
-              try {
-            
-                if (
-                  normalizedData.firstname.length > 1 &&
-                  normalizedData.lastname.length > 1 &&
-                  isValidEmail
-                ) {
-                  FilteredData.push({
-                    ...normalizedData,
-                    phone: normalizedData.phone || "",
-                    referral_amount: normalizedData.referral_amount || 0,
-                  });
-                } else {
-                  incorrectEmails.push(normalizedData);
-                  return; // Skip processing duplicate email
-                }
-              } catch (e) {
-                console.log("error", e);
-                return;
-              }
-            })
-            .on("end", async () => {
-
-
-             
-
-const promises = FilteredData.map(async (singleRow) => {
-  try {
-    const foundContact = await Contact.findOne({
-      email: singleRow.email,
-      user: userId,
-    });
-
-    if (foundContact) {
-      return {
-        status: "fulfilled",
-        value: {
-          email: singleRow.email,
-          firstname: singleRow.firstname,
-          lastname: singleRow.lastname,
-          phone: singleRow.phone,
-        },
-      };
-    } else {
-      let uniqueId = new mongoose.Types.ObjectId();
-      FinalList.push({
-        _id: uniqueId,
-        firstname: singleRow.firstname,
-        lastname: singleRow.lastname,
-        email: singleRow.email,
-        phone: singleRow.phone || "",
-        referral_link: `https://referme.uk/referral/${userId}/${uniqueId}`,
-        user: userId,
-        referral_amount: singleRow.referral_amount || 0,
-        date: currentDate.toString(),
-      });
-
-      return {
-        status: "fulfilled",
-        value: null,
-      };
-    }
-  } catch (error) {
-    return {
-      status: "rejected",
-      reason: error,
-    };
-  }
+app.get("/", async (req, res) => {
+  res.send("Hello world!");
 });
 
-              
+app.post("/addBulkContacts", async (req, res) => {
+  try {
+    const { userId } = req.query;
 
-              const results = await Promise.allSettled(promises);
-            
-              
-              results.forEach((result) => {
-              
-                if (result.status === "fulfilled" && result.value.value != null) {
-                
-                  incorrectEmails.push(result.value.value);
-                } else if (result.status === "rejected") {
-                  console.error("Error:", result.reason);
-                }
+    const issuer = await User.find({ _id: userId });
+
+    upload.single("file")(req, res, (error) => {
+      if (error) {
+        console.error("Error uploading file:", error);
+        return res.status(400).json({ message: "Error uploading file" });
+      }
+
+      if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+      }
+
+      const fileBuffer = req.file.buffer;
+
+      const stream = streamifier.createReadStream(fileBuffer);
+      const incorrectEmails = [];
+      const FinalList = [];
+      const FilteredData = [];
+      const currentDate = new Date();
+      const processedEmails = new Set();
+
+      stream
+        .pipe(csv())
+        .on("headers", async (headers) => {
+          const trimmedHeaders = headers.map((header) => header.trim());
+          const expectedHeaders = [
+            "firstname",
+            "lastname",
+            "email",
+            "phone",
+            "referral_amount",
+          ];
+          const missingHeaders = expectedHeaders.filter(
+            (header) => !trimmedHeaders.includes(header)
+          );
+
+          if (missingHeaders.length > 0) {
+            return res.status(400).json({
+              message:
+                "Column names in csv must match column names in csv temaplate",
+            });
+          }
+        })
+        .on("data", async (data) => {
+          // Normalize the data object keys by trimming them
+          const normalizedData = Object.fromEntries(
+            Object.entries(data).map(([key, value]) => [key.trim(), value])
+          );
+
+          if (processedEmails.has(normalizedData.email)) {
+            incorrectEmails.push(normalizedData);
+            return; // Skip processing duplicate email
+          }
+          processedEmails.add(normalizedData.email);
+          const isValidEmail = validator.validate(normalizedData.email);
+          try {
+            if (
+              normalizedData.firstname.length > 1 &&
+              normalizedData.lastname.length > 1 &&
+              isValidEmail
+            ) {
+              FilteredData.push({
+                ...normalizedData,
+                phone: normalizedData.phone || "",
+                referral_amount: normalizedData.referral_amount || 0,
+              });
+            } else {
+              incorrectEmails.push(normalizedData);
+              return; // Skip processing duplicate email
+            }
+          } catch (e) {
+            console.log("error", e);
+            return;
+          }
+        })
+        .on("end", async () => {
+          const promises = FilteredData.map(async (singleRow) => {
+            try {
+              const foundContact = await Contact.findOne({
+                email: singleRow.email,
+                user: userId,
               });
 
-           
+              if (foundContact) {
+                return {
+                  status: "fulfilled",
+                  value: {
+                    email: singleRow.email,
+                    firstname: singleRow.firstname,
+                    lastname: singleRow.lastname,
+                    phone: singleRow.phone,
+                  },
+                };
+              } else {
+                let uniqueId = new mongoose.Types.ObjectId();
+                FinalList.push({
+                  _id: uniqueId,
+                  firstname: singleRow.firstname,
+                  lastname: singleRow.lastname,
+                  email: singleRow.email,
+                  phone: singleRow.phone || "",
+                  referral_link: `https://referme.uk/referral/${userId}/${uniqueId}`,
+                  user: userId,
+                  referral_amount: singleRow.referral_amount || 0,
+                  date: currentDate.toString(),
+                });
 
-
-              let pendingContacts = FinalList.length;
-              const user = await User.findById(userId);
-              try {
-                user.pendingContactsToUpload = pendingContacts;
-                user.totalContactsBeforeUpload = user.contacts.length;
-                user.totalContactsToUpload = pendingContacts;
-                user.isUploadingContacts = true;
-                await user.save();
-              } catch (err) {
-            
-                return res
-                  .status(200)
-                  .json({ message: "Error in saving pending Contacts" });
+                return {
+                  status: "fulfilled",
+                  value: null,
+                };
               }
-
-      
-
-              res.status(200).json({
-                message: "Contacts added successfully",
-                incorrectEmails: incorrectEmails,
-              });
-
-              const contactDocuments = FinalList.map((row) => ({
-                  _id: row._id,
-                  firstname: row.firstname,
-                  lastname: row.lastname,
-                  email: row.email,
-                  phone: row.phone || "",
-                  referral_link: row.referral_link,
-                  user: row.user,
-                  referral_amount: row.referral_amount,
-                  date: row.date,
-              }));
-              
-              await Contact.insertMany(contactDocuments);
-
-
-              const contactIds = contactDocuments.map((row) => ({ contact_id: row._id }));
-            
-              
-              const userUpdate = {
-                $push: { contacts: { $each: contactIds } },
-                $set: {
-                  isUploadingContacts: false,
-                  pendingContactsToUpload: 0,
-                },
+            } catch (error) {
+              return {
+                status: "rejected",
+                reason: error,
               };
+            }
+          });
 
+          const results = await Promise.allSettled(promises);
 
-              await User.updateOne({ _id: userId }, userUpdate);
-            
+          results.forEach((result) => {
+            if (result.status === "fulfilled" && result.value.value != null) {
+              incorrectEmails.push(result.value.value);
+            } else if (result.status === "rejected") {
+              console.error("Error:", result.reason);
+            }
+          });
 
+          let pendingContacts = FinalList.length;
+          const user = await User.findById(userId);
+          try {
+            user.pendingContactsToUpload = pendingContacts;
+            user.totalContactsBeforeUpload = user.contacts.length;
+            user.totalContactsToUpload = pendingContacts;
+            user.isUploadingContacts = true;
+            await user.save();
+          } catch (err) {
+            return res
+              .status(200)
+              .json({ message: "Error in saving pending Contacts" });
+          }
 
-                     const msg = {
-                    to: `${issuer[0].email}`, // Change to your recipient
-                    from: {
-                      name: "ReferMe",
-                      email: "contact@referme.uk",
-                    }, // Change to your verified sender
-                    subject: `Your Contacts has been added Sucessfully`,
-                    html: `
+          res.status(200).json({
+            message: "Contacts added successfully",
+            incorrectEmails: incorrectEmails,
+          });
+
+          const contactDocuments = FinalList.map((row) => ({
+            _id: row._id,
+            firstname: row.firstname,
+            lastname: row.lastname,
+            email: row.email,
+            phone: row.phone || "",
+            referral_link: row.referral_link,
+            user: row.user,
+            referral_amount: row.referral_amount,
+            date: row.date,
+          }));
+
+          await Contact.insertMany(contactDocuments);
+
+          const contactIds = contactDocuments.map((row) => ({
+            contact_id: row._id,
+          }));
+
+          const userUpdate = {
+            $push: { contacts: { $each: contactIds } },
+            $set: {
+              isUploadingContacts: false,
+              pendingContactsToUpload: 0,
+            },
+          };
+
+          await User.updateOne({ _id: userId }, userUpdate);
+
+          const msg = {
+            to: `${issuer[0].email}`, // Change to your recipient
+            from: {
+              name: "ReferMe",
+              email: "contact@referme.uk",
+            }, // Change to your verified sender
+            subject: `Your Contacts has been added Sucessfully`,
+            html: `
                           <!DOCTYPE html>
                           <html>
                             <head>
@@ -310,29 +284,25 @@ const promises = FilteredData.map(async (singleRow) => {
                             </body>
                           </html>
                           `,
-                     };
+          };
 
-                  try {
-                    await sgMail.send(msg);
-                    console.log("Email sent");
-                  } catch (error) {
-                    console.error(error, "i am in the error of sending mail");
-                    // return res.send("failure");
-                  }
-
-
-
-            });
+          try {
+            await sgMail.send(msg);
+            console.log("Email sent");
+          } catch (error) {
+            console.error(error, "i am in the error of sending mail");
+            // return res.send("failure");
+          }
         });
-      }catch (e) {
-        res.status(400).send("failure");
-      }
-
+    });
+  } catch (e) {
+    res.status(400).send("failure");
+  }
 });
 
 app.get("/test", async (req, res) => {
-  console.log("I am working")
-  return res.status(200).json({message:"This endpoint working fine"})
-})
+  console.log("I am working");
+  return res.status(200).json({ message: "This endpoint working fine" });
+});
 
 module.exports = app;
